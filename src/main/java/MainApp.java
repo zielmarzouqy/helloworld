5677a3351385155d0bf3eddec8a21e27b1c18e21
zielmarzouqy <zi.elmarzouqy@gmail.com>
2020-02-06 00:37:02 +0100

    test 2

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -10,7 +10,7 @@ public class MainApp {
 	public static void main(String args[]) 
 	{
 		List<String> list = null;
-		// test local change dev-1
+		// test 2 dev-1
 //		@SuppressWarnings("resource")
 //		ApplicationContext appcntxt = new ClassPathXmlApplicationContext("applicationContext.xml");
 //		GeometryExample1 geo = appcntxt.getBean("geometryExample1", GeometryExample1.class);
